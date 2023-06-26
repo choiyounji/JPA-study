7921f46d85b5322dbcfd37fbc0e34c7ca5020eaa
choiyounji <yunji71@naver.com>
2023-06-26 16:58:55 +0900

    feat : follow chapter 2 example

---
+++ src/main/java/efub/study/jpa/JpaMain.java
@@ -0,0 +1,50 @@
+package efub.study.jpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("jpa-study");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction tx= em.getTransaction();
+
+        try{
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch(Exception e){
+            tx.rollback();
+        } finally{
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em){
+        String id="id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("윤지");
+        member.setAge(2);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(24);
+
+        //한 건 조회
+        Member findMember=em.find(Member.class, id);
+        System.out.println("findMember="+findMember.getUsername()+", age="+findMember.getAge());
+
+        //목록 조회
+        List<Member> members=em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("members.size="+members.size());
+
+        //삭제
+        em.remove(member);
+    }
+}
